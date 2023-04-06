0962ec33500590062bf7bec6a8bfd4393bea4853
Nihat <nihattagizade2003@gmail.com>
2023-04-06 13:12:50 +0400

    task

---
+++ task/script.js
@@ -0,0 +1,13 @@
+"use strict";
+
+function generatePassword() {
+    var length = document.getElementById("length").value;
+    var charset = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789!@#$%^&*_+-()?";
+    var password = "";
+    for (var i = 0; i < length; i++) {
+      var randomIndex = Math.floor(Math.random() * charset.length);
+      password += charset[randomIndex];
+    }
+    document.getElementById("password").value = password;
+}
+  
